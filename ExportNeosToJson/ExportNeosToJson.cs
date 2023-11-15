e1a4319f16048baa928c100d7b850ad0800d0fff
maksim789456 <maksim_789456@mail.ru>
2023-11-15 20:58:57 +0400

    Fix json, add Brson support

--- ExportNeosToJson/ExportNeosToJson.cs
+++ ExportNeosToJson/ExportNeosToJson.cs
@@ -34,36 +34,43 @@ namespace ExportNeosToJson
             modelFormats.Add("BSON");
             modelFormats.Add("7ZBSON");
             modelFormats.Add("LZ4BSON");
+            modelFormats.Add("BRSON");
             formatsField.SetValue(null, modelFormats.ToArray());
 
-            MethodInfo exportModelOriginal = AccessTools.DeclaredMethod(typeof(ModelExporter), nameof(ModelExporter.ExportModel), new Type[] {
-                typeof(Slot),
-                typeof(string),
-                typeof(Predicate<Component>) });
+            MethodInfo exportModelOriginal = AccessTools.DeclaredMethod(typeof(ModelExporter),
+                nameof(ModelExporter.ExportModel), new Type[]
+                {
+                    typeof(Slot),
+                    typeof(string),
+                    typeof(Predicate<Component>)
+                });
             if (exportModelOriginal == null)
             {
                 Error("Could not find ModelExporter.ExportModel(Slot, string, Predicate<Component>)");
                 return;
             }
-            MethodInfo exportModelPrefix = AccessTools.DeclaredMethod(typeof(ExportNeosToJson), nameof(ExportModelPrefix));
+
+            MethodInfo exportModelPrefix =
+                AccessTools.DeclaredMethod(typeof(ExportNeosToJson), nameof(ExportModelPrefix));
             harmony.Patch(exportModelOriginal, prefix: new HarmonyMethod(exportModelPrefix));
 
             Msg("Hook installed successfully");
         }
 
-        private static bool ExportModelPrefix(Slot slot, string targetFile, Predicate<Component> filter, ref Task<bool> __result)
+        private static bool ExportModelPrefix(Slot slot, string targetFile, Predicate<Component> filter,
+            ref Task<bool> __result)
         {
             string extension = Path.GetExtension(targetFile).Substring(1).ToUpper();
             SavedGraph graph;
             switch (extension)
             {
-                case "7ZBSON":
-                    graph = slot.SaveObject(DependencyHandling.CollectAssets);
-                    __result = Export7zbson(graph, targetFile);
-                    return false; // skip original function
                 case "JSON":
                     graph = slot.SaveObject(DependencyHandling.CollectAssets);
                     __result = ExportJson(graph, targetFile);
+                    return false; // skip original function
+                case "7ZBSON":
+                    graph = slot.SaveObject(DependencyHandling.CollectAssets);
+                    __result = Export7zbson(graph, targetFile);
                     return false; // skip original function
                 case "LZ4BSON":
                     graph = slot.SaveObject(DependencyHandling.CollectAssets);
@@ -73,6 +80,10 @@ namespace ExportNeosToJson
                     graph = slot.SaveObject(DependencyHandling.CollectAssets);
                     __result = ExportBson(graph, targetFile);
                     return false; // skip original function
+                case "BRSON":
+                    graph = slot.SaveObject(DependencyHandling.CollectAssets);
+                    __result = ExportBrson(graph, targetFile);
+                    return false; // skip original function
                 default:
                     return true; // call original function
             }
@@ -81,7 +92,15 @@ namespace ExportNeosToJson
         private static async Task<bool> ExportJson(SavedGraph graph, string targetFile)
         {
             await new ToBackground();
-            File.WriteAllText(targetFile, DataTreeConverter.ToJSON(graph.Root));
+            using (var fs = File.CreateText(targetFile))
+            {
+                var wr = new JsonTextWriter(fs);
+                wr.Indentation = 2;
+                wr.Formatting = Formatting.Indented;
+                var writeFunc = AccessTools.Method(typeof(DataTreeConverter), "Write");
+                writeFunc.Invoke(null, new object[] { graph.Root, wr });
+            }
+
             Msg(string.Format("exported {0}", targetFile));
             return true;
         }
@@ -91,8 +110,9 @@ namespace ExportNeosToJson
             await new ToBackground();
             using (FileStream fileStream = File.OpenWrite(targetFile))
             {
-                DataTreeConverter.ToBSON(graph.Root, fileStream);
+                DataTreeConverter.ToRawBSON(graph.Root, fileStream);
             }
+
             Msg(string.Format("exported {0}", targetFile));
             return true; // call original function
         }
@@ -104,6 +124,7 @@ namespace ExportNeosToJson
             {
                 DataTreeConverter.ToLZ4BSON(graph.Root, fileStream);
             }
+
             Msg(string.Format("exported {0}", targetFile));
             return true;
         }
@@ -115,6 +136,19 @@ namespace ExportNeosToJson
             {
                 DataTreeConverter.To7zBSON(graph.Root, fileStream);
             }
+
+            Msg(string.Format("exported {0}", targetFile));
+            return true;
+        }
+
+        private static async Task<bool> ExportBrson(SavedGraph graph, string targetFile)
+        {
+            await new ToBackground();
+            using (FileStream fileStream = File.OpenWrite(targetFile))
+            {
+                DataTreeConverter.ToBRSON(graph.Root, fileStream);
+            }
+
             Msg(string.Format("exported {0}", targetFile));
             return true;
         }
